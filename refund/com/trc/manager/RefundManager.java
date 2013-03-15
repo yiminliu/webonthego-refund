38ed9b04be128eb647daff16c8c38f266c83152c
yiminliu <yimin_l@hotmail.com>
2013-03-15 09:41:45 -0700

    added getPaymentTransaction()

--- refund/com/trc/manager/RefundManager.java
+++ refund/com/trc/manager/RefundManager.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Component;
 
 import com.trc.domain.refund.RefundCode;
 import com.trc.domain.refund.RefundRequest;
+import com.trc.exception.management.PaymentManagementException;
 import com.trc.exception.management.RefundManagementException;
 import com.trc.exception.service.RefundServiceException;
+import com.trc.service.PaymentService;
 import com.trc.service.RefundService;
 import com.trc.user.User;
 
+import com.tscp.mvne.PaymentTransaction;
 import com.tscp.util.logger.LogLevel;
 import com.tscp.util.logger.aspect.Loggable;
 
@@ -18,6 +21,8 @@ import com.tscp.util.logger.aspect.Loggable;
 public class RefundManager implements RefundManagerModel {
   @Autowired
   private RefundService refundService;  
+  @Autowired
+  PaymentService paymentService;
         
   @PreAuthorize("isAuthenticated() and hasPermission(#user, 'canRefund')")
   public void refundPayment(User user, RefundRequest refundRequest, int transId) throws RefundManagementException {
@@ -45,4 +50,12 @@ public class RefundManager implements RefundManagerModel {
         throw new RefundManagementException(e.getMessage(), e.getCause());
      }
   }    
+  
+  public PaymentTransaction getPaymentTransaction(int custId, int transId) throws RefundManagementException {
+	    try {
+	      return refundService.getPaymentTransaction(custId, transId);
+	    } catch (RefundServiceException e) {
+	    	throw new RefundManagementException(e.getMessage(), e.getCause());
+	    }
+	  }
 }
